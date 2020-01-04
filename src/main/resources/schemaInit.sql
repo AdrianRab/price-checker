2386b3eb0dc15b33e7120fae42c8c30ff7078433
Adrian <adrian.rabowski@gmail.com>
2020-01-04 14:27:29 +0100

    TUI-7 jpa dependency, connection to heroku posgres db

--- src/main/resources/schemaInit.sql
+++ src/main/resources/schemaInit.sql
@@ -1,3 +1,5 @@
+--CREATE SCHEMA IF NOT EXISTS price-checker
+
 --Drop table if exists room_details;
 --
 --create table room_details(
@@ -15,32 +17,3 @@
 --    details VARCHAR(250),
 --    originalPrice int
 --)
---
---
---
---    private String airportName;
---
---    private String departureDate;
---
---    private String returnDate;
---
---    private int duration;
---    @Getter @Setter
---    private String roomName;
---    @Getter @Setter
---    private String roomCode;
---    @Getter @Setter
---    private int price;
---    @Getter @Setter
---    private int discountPrice;
---    @Getter @Setter
---    private String offerCode;
---    private Date receivedOn;
---    @Getter @Setter
---    private String details;
---    @Getter @Setter
---    private int originalPrice;
---    @Getter @Setter
---    @ElementCollection
---    @CollectionTable(name="emails", joinColumns = @JoinColumn(name="id"))
---    private List<String> emails = new ArrayList<>();
